3f9f32290f3e77ed3bb48775e128102d3adb2c5a
Chandima Ranaweera <chan4lk@gmail.com>
2021-09-18 12:14:51 +0530

    Dapr Backend added.

--- DaprMultiContainer/DaprBackend/WeatherForecast.cs
+++ DaprMultiContainer/DaprBackend/WeatherForecast.cs
@@ -1,10 +1,13 @@
-﻿namespace DaprFrontEnd
+namespace DaprBackend
 {
     public class WeatherForecast
     {
         public DateTime Date { get; set; }
+
         public int TemperatureC { get; set; }
-        public int TemperatureF { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
         public string? Summary { get; set; }
     }
 }
