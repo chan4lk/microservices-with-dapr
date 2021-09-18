1be30efa3de65674826f27d73ad95066d5bb7827
Chandima Ranaweera <chan4lk@gmail.com>
2021-09-18 10:38:18 +0530

    Dapr counter added

---
+++ DaprCounter/Program.cs
@@ -0,0 +1,19 @@
+﻿using Dapr.Client;
+using System;
+using System.Threading.Tasks;
+
+Console.WriteLine("Hello, World!");
+
+var storeName = "statestore";
+var key = "counter";
+
+var client = new DaprClientBuilder().Build();
+var counter = await client.GetStateAsync<int>(storeName, key);
+
+while (true)
+{
+     Console.WriteLine($"Counter = {counter++}");
+
+     await client.SaveStateAsync(storeName, key, counter);
+     await Task.Delay(1000);
+}
